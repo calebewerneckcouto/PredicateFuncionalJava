bf1baab305beb2625e1dc886a44c4dabc5d730f2
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-07 12:48:27 -0300

    Expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,28 +3,27 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
-import util.ProductPredicate;
 
 public class Program {
-	
-	
-	public static void main(String []args) {
-		
+
+	public static void main(String[] args) {
+
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<Product>();
-		
-		list.add(new Product("Tv",900.00));
-		list.add(new Product("Mouse",50.00));
-		list.add(new Product("Tablet",350.50));
-		list.add(new Product("HD Case",80.90));
-		
-		
-		list.removeIf(Product::nonStaticProductPredicate);
-		
-		
-		for(Product p : list) {
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		Predicate<Product> pred = p -> p.getPrice() >= 100;
+
+		list.removeIf(pred);
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
 	}
