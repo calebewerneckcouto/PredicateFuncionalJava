1ba85ca1ba4274c4604aae37534883eb62ef400e
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-07 12:45:53 -0300

    Reference method com metodo nao estatico

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,7 @@ public class Program {
 		list.add(new Product("HD Case",80.90));
 		
 		
-		list.removeIf(Product::staticProductPredicate);
+		list.removeIf(Product::nonStaticProductPredicate);
 		
 		
 		for(Product p : list) {
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -3,17 +3,16 @@ package entities;
 import java.util.Objects;
 
 public class Product {
-	
-	
+
 	private String name;
 	private Double price;
-	
+
 	public Product() {
-	
+
 	}
 
 	public Product(String name, Double price) {
-		
+
 		this.name = name;
 		this.price = price;
 	}
@@ -55,10 +54,13 @@ public class Product {
 		Product other = (Product) obj;
 		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
 	}
-	
-	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice()>=100;
+
+	public static boolean staticProductPredicate(Product p) {//trabalha com argumento
+		return p.getPrice() >= 100;
+	}
+
+	public boolean nonStaticProductPredicate() {//nao usa argumento
+		return price >= 100;
 	}
 
 }
